0e035aa74e284a1561f9c0dd22b409cccee0973f
liqiuyu1997 <1600057738@qq.com>
2019-10-12 18:04:06 +0800

    完成登录问题最终版前端

---
+++ luckygou-admin-web/src/main.js
@@ -0,0 +1,62 @@
+import babelpolyfill from 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+//import './assets/theme/theme-green/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+//import NProgress from 'nprogress'
+//import 'nprogress/nprogress.css'
+import routes from './routes'
+
+//卸载mock
+// import Mock from './mock'
+// Mock.bootstrap();
+import 'font-awesome/css/font-awesome.min.css'
+
+//全局配置axios
+import axios from 'axios'
+axios.defaults.baseURL = "http://localhost:8991/services"//配置网关
+Vue.prototype.$http = axios
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+//NProgress.configure({ showSpinner: false });
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  //NProgress.start();
+    //如果将要跳转的是登录页面，将session中存储的user信息删除掉
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+  //从session中获取user信息
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  //！user 没有user没有登录过
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+new Vue({
+  //el: '#app',
+  //template: '<App/>',
+  router,
+  store,
+  //components: { App }
+  render: h => h(App)
+}).$mount('#app')
+
